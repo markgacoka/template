02be28e3519077cb421df8d841773476ded21d11
Mark Gacoka Mbui <markgacoka@gmail.com>
2024-10-09 20:00:06 -0700

    Add convex generated files

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,50 @@
+/* prettier-ignore-start */
+
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as auth from "../auth.js";
+import type * as index from "../index.js";
+import type * as migrations from "../migrations.js";
+import type * as posts from "../posts.js";
+import type * as tasks from "../tasks.js";
+import type * as users from "../users.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  index: typeof index;
+  migrations: typeof migrations;
+  posts: typeof posts;
+  tasks: typeof tasks;
+  users: typeof users;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+/* prettier-ignore-end */
